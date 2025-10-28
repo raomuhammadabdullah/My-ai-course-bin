ecbf0048e7f620e0a7809865b8419e736f4007d0
raomuhammadabdullah <abdullahraohafiz@gmail.com>
2025-10-28 19:49:46 +0500

    changing on age

--- student.py
+++ student.py
@@ -1,3 +1,20 @@
 studenthight = 5.9
 print(studenthight )
 print(type(studenthight))
+updatedstudenthight=studenthight + 3.5
+print(updatedstudenthight)
+print(type(updatedstudenthight))
+studentweight=int(input("enter your age"))
+print(studentweight)
+print(type(studentweight))
+print(studenthight==updatedstudenthight)
+print(studenthight!=studenthight)
+#variable=nethight=1.2
+nethight= float(1.2)
+print(type(nethight))
+name="rao"
+age=26
+hight=6
+print("my name is" + name)
+print("my age is" + str(age))
+print("my hight is" + str(hight))
