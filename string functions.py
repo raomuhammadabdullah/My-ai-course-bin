52a0e6b4a992692ac9bdeb9e16a48caa7e60f780
raomuhammadabdullah <abdullahraohafiz@gmail.com>
2025-10-30 20:39:45 +0500

    prractce on basic concepts

---
+++ string functions.py
@@ -0,0 +1,16 @@
+a="raoabdullah"
+print(a[1:4])#slicing function
+print(a[-3:-1])
+# string functions
+a="i am apitalize function"
+print(a.replace("a","b"))#replace function
+print(a.find("l"))#find function
+print(a.count("abdullah"))#count functionabdullah"
+print(a.endswith("lah"))#ends with function
+print(a.capitalize())#count function
+
+name=input("enter your name")
+b=print(name)
+print(len(name))
+name="hi i am 4$"
+print(name.count("$"))
--- student.py
+++ student.py
@@ -18,3 +18,26 @@ hight=6
 print("my name is" + name)
 print("my age is" + str(age))
 print("my hight is" + str(hight))
+a=2
+b=6
+print("the sum of variables",a+b)
+
+a=int(input("eneter 1 number"))
+b=int(input("enter your second number"))
+c=print("the sum is  ",int(a+b))
+a=float(input("enterr the 1 number"))
+b=float(input("enter yur 2 number"))
+c=print("their average is",(a+b)/2)
+a=int(input("enter your 1 number"))
+b=int(input("enter your 2 number"))
+print(a>=b)
+a="raoabdullah"
+print(a[1:4])#slicing function
+print(a[-3:-1])
+# string functions
+a="i am abdullah"
+print(a.endswith("lah"))#ends with function
+print(a.capitalize())#capitalize function
+print(a.replace("a","b"))#replace function
+print(a.find("l"))#find function
+print(a.count("abdullah"))#count function
