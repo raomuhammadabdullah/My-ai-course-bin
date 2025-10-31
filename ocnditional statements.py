ca445d1dd0056ca7d01ad9a854368ce05381cd1b
raomuhammadabdullah <abdullahraohafiz@gmail.com>
2025-10-31 22:32:50 +0500

    changing in code

---
+++ ocnditional statements.py
@@ -0,0 +1,17 @@
+marks=int(input("enter your marks"))
+if marks>=90:
+        print("you have secure grade A")
+elif marks>=80 and marks<90:
+        print("you have secure grade B")
+elif marks<80 and marks==70:
+        print("you have secure c grade")
+else:
+        print("you have secure d")  
+
+
+#if a number is even or odd
+a=int(input("enter a number"))
+if a/2:
+    print("the number is even")
+else:
+     print("the number is odd")      
--- string functions.py
+++ string functions.py
@@ -14,3 +14,34 @@ b=print(name)
 print(len(name))
 name="hi i am 4$"
 print(name.count("$"))
+a=int(input("enter your 1 number"))
+b=int(input("enter your 2 number"))
+c=int(input("enter your 3 number"))
+if a>b and a>c:
+    print("1 number is greate")
+elif b>a and b>c:
+    print("2 number is greater")
+else:
+    print("3 number is grater")
+
+    marks=int(input("enter your marks"))
+    if marks>=90:
+        print("you have secure grade A")
+    elif marks>=80 and marks<90:
+        print("you have secure grade B")
+    elif marks<80 and marks==70:
+        print("you have secure c grade")
+    else:
+        print("you have secure d")  
+
+    #program to check if a number is a multiple of 7 or not
+a= int(input("enter a number"))
+if a%7==0:
+  print("number is multiple of 7")
+else:
+    print("number is not multiple of 7")
+num=int(input("enter your age"))
+if num>=18:
+    print("you are eligible for driving")
+else:
+    print("you are not eligible for driving")    
